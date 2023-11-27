488ac3766bd9dcbb67b5cc849190df06530b75c0
Matteo Conti <matt.ciao@gmail.com>
2023-11-27 19:32:58 +0100

    fixed body

--- src/App.tsx
+++ src/App.tsx
@@ -12,49 +12,54 @@ function App() {
   const { t } = useTranslation();
 
   return (
-    <main>
-      <Navbar />
-      <HeroSection>
-        <h1 className="text-[#3ecf8e] text-6xl font-medium">
-          {t("home-title")}
-        </h1>
-        <h2 className="text-5xl font-medium">{t("home-subtitle")}</h2>
-      </HeroSection>
-      <Button>Hello</Button>
-      <div className="p-4 flex justify-center">
-        <ModeToggle />
-      </div>
-      <LanguageSwitcher />
-      <p>
-        <Trans i18nKey="description.part1">
-          Edit <code>src/App.js</code> and save to reload.
-        </Trans>
-      </p>
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        {t("description.part2")}
-      </a>
-      <p>{t("floritaly-description")}</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil totam
-        necessitatibus esse odit magni earum quod repellendus sunt, cum animi
-        doloribus amet molestiae minima. Architecto nulla quae neque quia porro?
-      </p>
+    <>
+      <header className="sticky top-0 z-20">
+        <Navbar />
+      </header>
+      <main>
+        <HeroSection>
+          <h1 className="text-[#3ecf8e] text-6xl font-medium">
+            {t("home-title")}
+          </h1>
+          <h2 className="text-5xl font-medium">{t("home-subtitle")}</h2>
+        </HeroSection>
+        <Button>Hello</Button>
+        <div className="p-4 flex justify-center">
+          <ModeToggle />
+        </div>
+        <LanguageSwitcher />
+        <p>
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          {t("description.part2")}
+        </a>
+        <p>{t("floritaly-description")}</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil totam
+          necessitatibus esse odit magni earum quod repellendus sunt, cum animi
+          doloribus amet molestiae minima. Architecto nulla quae neque quia
+          porro?
+        </p>
 
-      <h1 className="text-9xl">hello worldy</h1>
-      <div className="overflow-hidden">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          animate={{ x: 100 }}
-          transition={{ duration: 2 }}
-        >
-          hello world
-        </motion.div>
-      </div>
-      <div className="p-4 flex justify-center">
-        <ModeToggle />
-      </div>
-    </main>
+        <h1 className="text-9xl">hello worldy</h1>
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            animate={{ x: 100 }}
+            transition={{ duration: 2 }}
+          >
+            hello world
+          </motion.div>
+        </div>
+        <div className="p-4 flex justify-center">
+          <ModeToggle />
+        </div>
+      </main>
+    </>
   );
 }
 
