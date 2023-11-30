641fa775d933c559fbf2154dfd799bae2ac33788
Matteo Conti <matt.ciao@gmail.com>
2023-11-30 16:47:05 +0100

    moved content to hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,19 @@
-import { motion } from "framer-motion";
-import { useTranslation, Trans } from "react-i18next";
 import { Suspense } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/toggle-light-dark";
-import { Button } from "@/components/ui/button";
+
 import Navbar from "./components/navbar";
 import HeroSection from "./components/hero";
 import Grid from "./components/discover";
 import Footer from "./components/footer";
-import LanguageSwitcher from "@/components/toggle-language";
-import { Rat } from "lucide-react";
 
 function App() {
-  const { t, i18n } = useTranslation();
-
   return (
     <>
       <header className="sticky top-0 z-20 border-b border-input shadow-sm">
         <Navbar />
       </header>
       <main className="container">
-        <HeroSection>
-          {i18n.resolvedLanguage === "en" && (
-            <h1 className="text-[#3ecf8e]  text-title font-medium text-center md:text-left">
-              {t("home-title")}
-            </h1>
-          )}
-          <h2 className=" text-title font-medium text-center md:text-left">
-            {t("home-subtitle.part1")}
-          </h2>
-          <h2 className=" text-title font-medium text-center md:text-left">
-            {t("home-subtitle.part2")}
-          </h2>
-          {i18n.resolvedLanguage === "it" && (
-            <h1 className="text-[#3ecf8e]  text-title font-medium text-center md:text-left">
-              {t("home-title")}
-            </h1>
-          )}
-          <p className="py-2 text-center max-w-md md:text-justify md:pl-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            repellendus sit quia commodi illum quod voluptatibus? Reprehenderit,
-            odio consequatur. Magnam vero laudantium perferendis provident
-            facilis aperiam omnis quam, fuga corporis!
-          </p>
-          <div className="md:pl-1 py-2">
-            <Button>Risorse </Button>
-          </div>
-        </HeroSection>
+        <HeroSection></HeroSection>
         <Grid />
       </main>
       <Footer />
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,12 +1,37 @@
-// a hero with a title and a subtitle and image on the right
-export default function HeroSection({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { Button } from "./ui/button";
+import { useTranslation } from "react-i18next";
+export default function HeroSection() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="flex justify-evenly flex-col md:flex-row">
-      <div className="mt-20 text-justify md:text-left">{children}</div>
+      <div className="mt-20 text-justify md:text-left">
+      {i18n.resolvedLanguage === "en" && (
+            <h1 className="text-[#3ecf8e]  text-title font-medium text-center md:text-left">
+              {t("home-title")}
+            </h1>
+          )}
+          <h2 className=" text-title font-medium text-center md:text-left">
+            {t("home-subtitle.part1")}
+          </h2>
+          <h2 className=" text-title font-medium text-center md:text-left">
+            {t("home-subtitle.part2")}
+          </h2>
+          {i18n.resolvedLanguage === "it" && (
+            <h1 className="text-[#3ecf8e]  text-title font-medium text-center md:text-left">
+              {t("home-title")}
+            </h1>
+          )}
+          <p className="py-2 text-center max-w-md md:text-justify md:pl-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            repellendus sit quia commodi illum quod voluptatibus? Reprehenderit,
+            odio consequatur. Magnam vero laudantium perferendis provident
+            facilis aperiam omnis quam, fuga corporis!
+          </p>
+          <div className="md:pl-1 py-2">
+            <Button>Risorse </Button>
+          </div>
+      </div>
       <div className="py-12 md:h-[40rem] object-contain overflow-hidden flex justify-center">
       <img src="https://assets.codepen.io/2621168/svgtest.svg" alt="Phone illustration" className="md:mt-40"/>
         {/* <img className="max-w-[200px] max-h-[300px]" src="plantrm.png" alt="hero"></img> */}
