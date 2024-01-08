78ba5c225484d850c32f64667a43c50a5ed888de
Matteo Conti <matt.ciao@gmail.com>
2024-01-08 12:33:15 +0100

    added sites images

--- src/components/discover.tsx
+++ src/components/discover.tsx
@@ -18,17 +18,14 @@ export default function DiscoverSection() {
         <div className=" p-12 relative flex justify-center items-center ">
           <CircleLogo />
         </div>
-        <div
-          className="flex flex-col max-w-[500px] gap-4 md:gap-2 -mt-[100px] md:mt-0"
-  
-        >
+        <div className="flex flex-col max-w-[500px] gap-4 md:gap-2 -mt-[100px] md:mt-0">
           <h3 className="text-4xl text-center md:text-left">Lorem title</h3>
           <p>
             {" "}
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
             mollitia reiciendis rem molestias. Quod iusto amet minima sapiente
             nisi quidem vero? Fuga similique fugiat labore cupiditate voluptas
-            sapiente explicabo sed.
+            sapiente explicabo sedo.
           </p>
           <div className="flex justify-center md:justify-start  py-2">
             <Button>Scopri</Button>
@@ -37,11 +34,7 @@ export default function DiscoverSection() {
       </div>
       {/* SECTION 2 */}
       <div className="min-w-full flex flex-col-reverse md:flex-row justify-evenly items-center gap-4 my-32">
-        
-        <div
-          className="flex flex-col max-w-[500px] gap-4 md:gap-2 -mt-[100px] md:mt-0"
-         
-        >
+        <div className="flex flex-col max-w-[500px] gap-4 md:gap-2 -mt-[100px] md:mt-0">
           <h3 className="text-4xl text-center md:text-left">Lorem title</h3>
           <p>
             {" "}
@@ -61,12 +54,9 @@ export default function DiscoverSection() {
       {/* SECTION 3 */}
       <div className="min-w-full flex flex-col md:flex-row justify-evenly items-center gap-4 md:my-60 mb-40">
         <div className="p-0 md:p-12 relative flex justify-center items-center ">
-          <Globe className="w-[100px] h-[100px] text-[#3ecf8e] dark:text-[#3ecf8e]"/>
+          <Globe className="w-[100px] h-[100px] text-[#3ecf8e] dark:text-[#3ecf8e]" />
         </div>
-        <div
-          className="flex flex-col max-w-[500px] gap-4 md:gap-2 "
-        
-        >
+        <div className="flex flex-col max-w-[500px] gap-4 md:gap-2 ">
           <h3 className="text-4xl text-center md:text-left">Lorem title</h3>
           <p>
             {" "}
--- src/components/ui/circle.tsx
+++ src/components/ui/circle.tsx
@@ -4,55 +4,55 @@ export default function CircleLogo() {
   return (
     <div className="circle-perimeter">
       <div className="pulse animate-ping"></div>
-      <div className="logo-center pulsating-circle flex items-center justify-center bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1 ">
+      <div className="logo-center pulsating-circle flex items-center justify-center bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1 ">
         <Leaf className=" w-8 h-8 text dark:filter dark:brightness-0 dark:invert"/>
       </div>
       {/* <div className="logo-center flex items-center bg-pure rounded-full w-[70px] h-[70px] shadow-lg p-1 ">
         <img className="dark:filter dark:brightness-0 dark:invert" src="./loghi/lw-ita.png" alt="" />
       </div> */}
-      <div className="perimeter-logo logo-pos-0 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-0 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert"
           src="./loghi/sapienza.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-1 bg-pure rounded-full  w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-1 bg-pure rounded-full  w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="filter brightness-0 dark:invert"
           src="./loghi/units-small.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-2 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-2 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert"
           src="./loghi/lw-ita.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-3 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-3 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert"
           src="./loghi/unibo-small.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-4 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-4 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert"
           src="./loghi/pisa.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-5 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-5 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert"
           src="./loghi/perugia.png"
           alt=""
         />
       </div>
-      <div className="perimeter-logo logo-pos-6 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg p-1">
+      <div className="perimeter-logo logo-pos-6 bg-pure rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px] shadow-lg dark:shadow-muted p-1">
         <img
           className="dark:filter dark:brightness-0 dark:invert max-w-[90%] mt-2"
           src="./loghi/camerino2.png"
