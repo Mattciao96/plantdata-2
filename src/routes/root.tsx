1311ef0c140ea17bddc437ec913c9061fcee56f5
Matteo Conti <matt.ciao@gmail.com>
2024-01-09 10:54:08 +0100

    Add ScrollToTop component

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,15 +2,31 @@ import Navbar from "@/components/navbar";
 
 import Footer from "@/components/footer";
 import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 
 export default function Root() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
+      
       <main className="container min-h-[calc(100vh-3.4rem)]">
         <Outlet/>
       </main>
       <Footer />
     </>
   );
+}
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
