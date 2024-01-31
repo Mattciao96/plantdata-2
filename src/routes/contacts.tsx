c63244279a3d2a8f5b6bf076df4b16d426721a08
Matteo Conti <matt.ciao@gmail.com>
2024-01-31 15:03:10 +0100

    Refactor contacts route and add contact information

--- src/routes/contacts.tsx
+++ src/routes/contacts.tsx
@@ -1,6 +1,22 @@
-import UniversityList from "@/components/university-list";
 import { Helmet } from "react-helmet";
+import Title from "@/components/ui/title";
+import { useTranslation } from "react-i18next";
+
+const contacts: ContactProps[] = [
+  {
+    role: "contacts.people.0.role",
+    name: "contacts.people.0.name",
+    email: "contacts.people.0.email",
+  },
+  {
+    role: "contacts.people.1.role",
+    name: "contacts.people.1.name",
+    email: "contacts.people.1.email",
+  },
+];
+
 export default function Contacts() {
+  const { t } = useTranslation();
   return (
     <>
       <Helmet>
@@ -11,7 +27,24 @@ export default function Contacts() {
         />
         <link rel="icon" href="" />
       </Helmet>
-      <UniversityList />
+
+      <Title text={"contacts.title"} />
+      <ul className="flex flex-col items-center gap-4">
+        {contacts.map((contact) => (
+          <li className="max-w-[300px]">
+            <h3 className="=text-center text-4xl md:mb-2 md:max-w-full ">
+              {t(contact.role)}
+            </h3>
+            <div>
+              <p className="text-center text-[14px] md:mb-4 md:pl-1">
+                {t(contact.name)}
+                <br />
+                {t(contact.email)}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
