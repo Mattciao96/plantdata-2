f3a3bd5eea15b4a618f4dc4dd9a8412e3184629d
Matteo Conti <matt.ciao@gmail.com>
2024-02-19 10:46:08 +0100

    Refactor error message handling

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -5,13 +5,20 @@ export default function ErrorPage() {
   const error = useRouteError();
   console.log(error);
 
+  let errorMessage = 'An unknown error occurred';
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === 'object' && error !== null && 'statusText' in error) {
+    errorMessage = (error as { statusText: string }).statusText;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[100vh]">
       <div>
         <h1>404</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>
-          <i>{error.statusText || error.message}</i>
+          <i>{errorMessage}</i>
         </p>
         <Rat className="fixed bottom-4 right-4 w-5 h-5" />
       </div>
