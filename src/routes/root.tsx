3ca22ba0c0556182599be172d4eaf8843313097c
Matteo Conti <matt.ciao@gmail.com>
2024-02-19 10:52:38 +0100

    Remove unnecessary import

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,18 +1,14 @@
 import Navbar from "@/components/navbar";
-
 import Footer from "@/components/footer";
 import { Outlet } from "react-router-dom";
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
-
 
 export default function Root() {
   return (
     <>
       <ScrollToTop />
       <Navbar />
-      
       <main className="container-fluid min-h-[calc(100vh-3.4rem)]">
         <Outlet/>
       </main>
