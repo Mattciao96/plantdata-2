e5206a7207a6a8edeff4bb9209e5bc640a1a2746
Matteo Conti <matt.ciao@gmail.com>
2024-02-19 11:14:16 +0100

    add email links

--- src/routes/contacts.tsx
+++ src/routes/contacts.tsx
@@ -2,6 +2,12 @@ import { Helmet } from "react-helmet";
 import Title from "@/components/ui/title";
 import { useTranslation } from "react-i18next";
 
+interface ContactProps {
+  role: string;
+  name: string;
+  email: string;
+}
+
 const contacts: ContactProps[] = [
   {
     role: "contacts.people.0.role",
@@ -29,23 +35,23 @@ export default function Contacts() {
       </Helmet>
 
       <Title text={"contacts.title"} />
-      {/* <ul className="flex flex-col items-center gap-4">
-        {contacts.map((contact) => (
-          <li className="max-w-[300px]">
-            <h3 className="=text-center text-4xl md:mb-2 md:max-w-full ">
+      <ul className="flex flex-col items-center gap-4">
+        {contacts.map((contact, index) => (
+          <li className="max-w-[300px]" key={index}>
+            <h3 className="text-center text-2xl md:mb-2 md:max-w-full ">
               {t(contact.role)}
             </h3>
             <div>
               <p className="text-center text-[14px] md:mb-4 md:pl-1">
                 {t(contact.name)}
                 <br />
-                {t(contact.email)}
+                <a className="text-blue-600 dark:text-blue-500 hover:underline" href={`mailto:${t(contact.email)}`}>{t(contact.email)}</a>
               </p>
             </div>
           </li>
         ))}
-      </ul> */}
-      <div className="flex justify-center">Coming soon</div>
+      </ul>
+      {/* <div className="flex justify-center">Coming soon</div> */}
     </>
   );
 }
