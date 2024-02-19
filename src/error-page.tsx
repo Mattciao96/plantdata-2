eece9e230cb28f9383b031bbbff52ca2d48e1c1c
Matteo Conti <matt.ciao@gmail.com>
2024-02-19 10:49:30 +0100

    Refactor error-page component layout and styles

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -5,22 +5,26 @@ export default function ErrorPage() {
   const error = useRouteError();
   console.log(error);
 
-  let errorMessage = 'An unknown error occurred';
+  let errorMessage = "An unknown error occurred";
   if (error instanceof Error) {
     errorMessage = error.message;
-  } else if (typeof error === 'object' && error !== null && 'statusText' in error) {
+  } else if (
+    typeof error === "object" &&
+    error !== null &&
+    "statusText" in error
+  ) {
     errorMessage = (error as { statusText: string }).statusText;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-[100vh]">
+    <div className="flex min-h-[100vh] w-full flex-col items-center justify-center">
       <div>
-        <h1>404</h1>
+        <h1 className="mb-4 text-5xl">404</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>
           <i>{errorMessage}</i>
         </p>
-        <Rat className="fixed bottom-4 right-4 w-5 h-5" />
+        <Rat className="fixed bottom-4 right-4 h-5 w-5" />
       </div>
     </div>
   );
