5f74149521fc6d70a79a451e6dfcc29fd084477f
Matteo Conti <matt.ciao@gmail.com>
2024-02-19 11:29:58 +0100

    Refactor resource component and add type checking

--- src/components/resource.tsx
+++ src/components/resource.tsx
@@ -3,8 +3,15 @@ import { useTranslation } from "react-i18next";
 import Title from "./ui/title";
 import { Link } from "react-router-dom";
 
-// data from locales translation.json
-const resourcesData = [
+interface ResourceData {
+  title: string;
+  image: string;
+  description: string;
+  link: string;
+  button: string;
+}
+
+const resourcesData: ResourceData[] = [
   {
     title: "resources.floritaly.title",
     image: "images/sites/floritalylogo.png",
@@ -31,46 +38,37 @@ const resourcesData = [
 export default function ResourceList() {
   return (
     <>
-      <Title text={"resources.title"}/>
-      <div className="flex w-full flex-col items-center">
+      <Title text={"resources.title"} />
+      <ul className="flex w-full flex-col items-center">
         {resourcesData.map((resource) => (
-          <Resource
-            title={resource.title}
-            description={resource.description}
-            image={resource.image}
-            link={resource.link}
-            button={resource.button}
-            key={resource.title}
-          />
+          <Resource {...resource} key={resource.title} />
         ))}
-      </div>
+      </ul>
     </>
   );
 }
 
-function Resource({ title, description, image, link, button }) {
+function Resource({ title, description, image, link, button }: ResourceData) {
   const { t } = useTranslation();
   return (
-    <div className="mb-20 flex w-full max-w-[400px] flex-col items-center justify-evenly gap-4 rounded-xl border border-input bg-pure p-6 first:mt-0 last:mb-16 md:my-8 md:max-w-[900px] md:flex-row">
+    <li className="mb-20 flex w-full max-w-[400px] flex-col items-center justify-evenly gap-4 rounded-xl border border-input bg-pure p-6 first:mt-0 last:mb-16 md:my-8 md:max-w-[900px] md:flex-row">
       <div className="relative flex items-center justify-evenly p-0 py-4 md:px-12 ">
         {/* <Globe className="w-[100px] h-[100px] text-[#3ecf8e] dark:text-[#3ecf8e]" /> */}
         <img
           src={image}
           alt={t(title)}
-          className="h-[200px] w-[200px] object-contain"
+          className="h-[200px] w-[200px] rounded-md object-fill"
         />
       </div>
       <div className="flex flex-col gap-4 md:w-[60%] md:gap-2 md:px-4">
         <h3 className="mb-4 text-center text-4xl md:text-left">{t(title)}</h3>
         <p className="pl-[2px]">{t(description)}</p>
         <div className="flex justify-center py-2 md:justify-start">
-          <a href={link}>
-            <Button>
-              <Link to={link}>{t(button)}</Link>
-            </Button>
-          </a>
+          <Button asChild>
+            <Link to={link}>{t(button)}</Link>
+          </Button>
         </div>
       </div>
-    </div>
+    </li>
   );
 }
